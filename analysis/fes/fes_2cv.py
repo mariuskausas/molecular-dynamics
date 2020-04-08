332e69a68219bdce41187cf8c904d42abc34886a
mariuskausas <kausas.m@gmail.com>
2020-04-08 17:06:42 +0100

    Updated FES for two collective variables

--- analysis/fes/fes_2cv.py
+++ analysis/fes/fes_2cv.py
@@ -5,7 +5,7 @@ Generate a 2D free energy surface of two collective variables.
 Before use:
 - Install numpy and matplotlib
 
-Marius Kausas					   2019 11 18
+Marius Kausas					   2020 04 08
 =============================================
 """
 
@@ -14,55 +14,182 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def prepare_two_cvs(cv1, cv2, bins, weights=None):
-    """ 2D histogram two collective variables and prepare input for FES plot.
-
-    Weights can be provided to weight the resulted 2D histogram. 
-    cv1, cv2 and weights should be provided as (n,) numpy arrays.
+def get_histogram(xval, yval, nbins, weights):
     """
-    
-    # Define range of collective variables
-    xmin = np.floor(cv1.min())
-    xmax = np.ceil(cv1.max())
-    ymin = np.floor(cv2.min())
-    ymax = np.ceil(cv2.max())
+    Compute 2D histogram of two variables.
 
-    # Calculate bin size for each collective variable
-    xbin_size = (xmax - xmin) / bins
-    ybin_size = (ymax - ymin) / bins
-    xedges = np.arange(xmin, xmax, xbin_size)
-    yedges = np.arange(ymin, ymax, ybin_size)
+    Parameters
+    ----------
+    xval : ndarray
+        Numpy array (N, ) of the first variable.
+    yval : ndarray
+        Numpy array (N, ) of the first variable.
+    nbins : int
+        Number of bins.
+    weights : ndarray
+        Numpy array (N, ) of weights.
+
+    Returns
+    -------
+    z : ndarray
+        Numpy array (N, N) of the histogram.
+    xedges : ndarray
+        Numpy array (N, ) of the x bin edges.
+    yedges : ndarray
+        Numpy array (N, ) of the y bin edges.
+    """
+
+    z, x, y = np.histogram2d(xval, yval, bins=nbins, weights=weights)
+    xedges = 0.5 * (x[:-1] + x[1:])
+    yedges = 0.5 * (y[:-1] + y[1:])
+
+    return z.T, xedges, yedges
+
+
+def density(z):
+    """
+    Compute a probability density function.
+
+    Parameters
+    ----------
+    z : ndarray
+        Numpy array (N, N) of the histogram.
+
+    Returns
+    -------
+    out : ndarray
+        Numpy array (N, N) of the probability density function
+    """
+
+    return z / float(z.sum())
+
+
+def free_energy(z):
+    """
+    Transform probability density function into free energy.
+
+    Parameters
+    ----------
+    z : ndarray
+        Numpy array (N, N) of the probability density function
+
+    Returns
+    -------
+    out : ndarray
+        Numpy array (N, N) of the free energy surface values.
+    """
+
+    prob = density(z)
+    free_energy = np.inf * np.ones(shape=z.shape)
+    nonzero = prob.nonzero()
+    free_energy[nonzero] = -np.log(prob[nonzero])
+
+    return free_energy
+
+
+def prepare_two_cvs(path_to_cv1, path_to_cv2, nbins, weights):
+    """
+    Convert two collective variables into a free energy surface.
+
+    Parameters
+    ----------
+    path_to_cv1 : str
+        Path to the file containing first collective variable values.
+    path_to_cv2 : str
+        Path to the file containing second collective variable values.
+    nbins : int
+        Number of bins.
+    weights : ndarray
+        Numpy array (N, ) of weights.
+
+    Returns
+    -------
+    f : ndarray
+        Numpy array (N, N) of the free energy surface values.
+    xedges : ndarray
+        Numpy array (N, ) of the x bin edges.
+    yedges : ndarray
+        Numpy array (N, ) of the y bin edges.
+    """
+
+    # Load collective variables
+    cv1 = np.loadtxt(path_to_cv1)
+    cv2 = np.loadtxt(path_to_cv2)
 
     # Histogram values into 2D array
-    H, xedges, yedges = np.histogram2d(cv1, cv2, bins=(xedges, yedges), weights=weights, normed=True)
+    H, xedges, yedges = get_histogram(cv1, cv2, nbins, weights)
+    f = free_energy(H)
 
-    return -np.log(H.T), xedges, yedges
+    return f, xedges, yedges
 
 
-def plot_2D_fes(H, xedges, yedges, output_name):
-    """ Helper plotting function."""
-    
+def plot_2D_contour(f, xedges, yedges, cv1_label, cv2_label, output_name):
+    """ Plotting function for 2D free energy surface."""
+
     fs = 22
-    fig = plt.figure(figsize=[6, 4])
+    fig = plt.figure(figsize=[10, 8])
     ax = fig.add_subplot(111)
-    
-    imshow = ax.imshow(H, interpolation='nearest', origin='low',
-            extent=[xedges[0], xedges[-1], yedges[0], yedges[-1]], aspect="auto", cmap="Spectral")
-    fig.colorbar(imshow, ax = ax)
-    
+
+    # Normalize free energy surface to 0
+    f -= f.min()
+    # Extract maximal FES value for contour level plotting
+    m = f[np.isfinite(f)].max()
+
+    # Filled contour plot
+    contour = ax.contourf(f, extent=[xedges[0], xedges[-1], yedges[0], yedges[-1]],
+                          cmap="Spectral",
+                          levels=np.linspace(0, np.round(m), np.round(m) * 2 + 1),
+                          corner_mask=True,
+                          alpha=0.75)
+    cbar = fig.colorbar(contour, ax=ax)
+    cbar.ax.tick_params(labelsize=fs)
+    cbar.ax.set_ylabel("$-\ln P$ MD", fontsize=fs)
+
+    # Contour outline plot
+    contour = ax.contour(f, extent=[xedges[0], xedges[-1], yedges[0], yedges[-1]],
+                         cmap="gist_gray",
+                         levels=np.linspace(0, np.round(m), np.round(m) * 2 + 1),
+                         linewidths=1,
+                         corner_mask=True,
+                         alpha=1)
+
+    ax.tick_params(labelsize=fs)
+
+    ax.set_xlabel(cv1_label, fontsize=fs)
+    ax.set_ylabel(cv2_label, fontsize=fs)
+
     fig.tight_layout()
-    plt.savefig(output_name + ".png", dpi=600)
-    plt.show()
+    plt.savefig(output_name + ".png", dpi=300)
+    plt.close()
 
 
-def two_variable_fes(cv1, cv2, bins, output_name, weights=None):
-    """ Plot 2D FES."""
+def two_variable_fes(path_to_cv1, path_to_cv2, nbins, cv1_label, cv2_label, output_name, weights=None):
+    """
+    Plot 2D free energy function.
+
+    Parameters
+    ----------
+    path_to_cv1 : str
+        Path to the file containing first collective variable values.
+    path_to_cv2 : str
+        Path to the file containing second collective variable values.
+    nbins : int
+        Number of bins.
+    cv1_label : str
+       Label of the first collective variable.
+    cv2_label : str
+        Label of the second collective variable.
+    output_name : str
+        Name of the output figure.
+    weights : str
+       Path to the file containing weights.
+    """
 
     # Check if the weights are provided
     if weights is not None:
-        weights = load_weights(weights)
+        weights = np.loadtxt(weights)
 
-    H, xedges, yedges = prepare_two_cvs(cv1, cv2, bins, weights)
-    plot_2D_fes(H, xedges, yedges, output_name)
+    f, xedges, yedges = prepare_two_cvs(path_to_cv1, path_to_cv2, nbins, weights)
+    plot_2D_contour(f, xedges, yedges, cv1_label, cv2_label, output_name)
 
     return
